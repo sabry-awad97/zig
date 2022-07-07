cedd849ba425e086aee75bab9df933debd71bdfa
sabry-awad97 <96271458+sabry-awad97@users.noreply.github.com>
2022-07-07 03:32:03 +0200

    fn with args

---
+++ fn-args.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn main() void {
+    std.debug.print("Powers of two: {} {} {} {}\n", .{
+        twoToThe(1),
+        twoToThe(2),
+        twoToThe(3),
+        twoToThe(4),
+    });
+}
+
+fn twoToThe(my_number: u32) u32 {
+    return std.math.pow(u32, 2, my_number);
+    // std.math.pow(type, a, b) takes a numeric type and two
+    // numbers of that type (or that can coerce to that type) and
+    // returns "a to the power of b" as that same numeric type.
+}
