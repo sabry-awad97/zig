8def708aed81c266027fed0ef3289e92ab754a15
sabry-awad97 <96271458+sabry-awad97@users.noreply.github.com>
2022-07-07 02:41:52 +0200

    multi-line

---
+++ multi-line-string.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() void {
+    const lyrics =
+        \\Ziggy played guitar
+        \\Jamming good with Andrew Kelley
+        \\And the Spiders from Mars
+    ;
+
+    std.debug.print("{s}\n", .{lyrics});
+}
