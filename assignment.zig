f3fea9347feb4171ab201aa834b93899599c5a5b
sabry-awad97 <96271458+sabry-awad97@users.noreply.github.com>
2022-07-07 02:17:22 +0200

    var and const

---
+++ assignment.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+pub fn main() void {
+    var n: u8 = 50;
+    n = n + 5;
+
+    const pi: u32 = 314159;
+
+    const negative_eleven: i8 = -11;
+
+    // the first parameter is a string.
+    // The string may contain placeholders '{}', and the
+    // second parameter is an "anonymous list literal".
+    std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
+}
