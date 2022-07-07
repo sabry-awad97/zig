3a11102cc331b33786f2c34b74d39e8ee920fe3f
sabry-awad97 <96271458+sabry-awad97@users.noreply.github.com>
2022-07-07 03:24:21 +0200

    fizz-buzz

---
+++ fizz-buzz.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn main() void {
+    var i: u8 = 1;
+    var stop_at: u8 = 16;
+
+    while (i <= stop_at) : (i += 1) {
+        if (i % 3 == 0) std.debug.print("Fizz", .{});
+        if (i % 5 == 0) std.debug.print("Buzz", .{});
+        if (!(i % 3 == 0) and !(i % 5 == 0)) {
+            std.debug.print("{}", .{i});
+        }
+        std.debug.print(", ", .{});
+    }
+    std.debug.print("\n", .{});
+}
